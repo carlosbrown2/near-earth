35004ab29aa35aca4af93075007493bccb8bca81
Carlos Brown <carlos.brown3@gmail.com>
2024-08-13 17:24:53 -0600

    clean up for merge

--- distance.py
+++ distance.py
@@ -1,5 +1,6 @@
 from astroquery.jplhorizons import Horizons
 import astropy.units as u
+from astropy.table import Table
 import matplotlib.pyplot as plt
 import pdb
 from datetime import date, timedelta
@@ -7,7 +8,7 @@ import pandas as pd
 import re
 from tqdm import tqdm
 from numpy import argmin
-from typing import List, Dict
+from typing import List, Dict, AnyStr
 
 
 
@@ -18,14 +19,14 @@ epoch_length = 365 * years
 
 
 class Distance:
-    def __init__(self, spkids, location, epoch_length) -> None:
+    def __init__(self, spkids: List, location: AnyStr, epoch_length: int) -> None:
         self.spkids = spkids
         self.location = location
         self.epoch_length = epoch_length
         self.today = date.today().strftime('%Y-%m-%d')
-        self.future_date = (date.today() + timedelta(days=epoch_length)).strftime('%Y-%m-%d')    
+        self.future_date = (date.today() + timedelta(days=epoch_length)).strftime('%Y-%m-%d')
 
-    def calculate_ephemeris(self, spkid):
+    def calculate_ephemeris(self, spkid: AnyStr) -> Table:
         neo = Horizons(id=spkid, location=self.location, id_type='designation', epochs={'start': self.today, 'stop': self.future_date, 'step': '1d'})
         try:
             neo_ephem = neo.ephemerides()
@@ -50,7 +51,7 @@ class Distance:
     def check_approaches(self) -> List[Dict]:
         bad_neos = []
         for spkid in tqdm(self.spkids):
-            neo_ephem = self.calculate_ephemerid(spkid)
+            neo_ephem = self.calculate_ephemeris(spkid)
                 # Calculate the distance between NEO and Earth
             distance = neo_ephem['delta'].to(u.km) - neo_ephem['r_3sigma']# - earth_ephem['delta'].to(u.km)
             min_dist = distance.min()
@@ -60,8 +61,20 @@ class Distance:
                 t = neo_ephem['datetime_jd'][idx]
                 bad_neos.append({'spkid':spkid, 'distance': min_dist.value, 'time': t})
         return bad_neos
+    
+    def plot_ephemeris(self, ephem: Table, feature: AnyStr, title: AnyStr) -> None:
+        # Plot the distance
+        fig, ax = plt.subplots()
+        ax.plot(ephem['datetime_jd'].to(u.km), ephem[feature], 'k')
+        ax.set_xlabel('Time (JD)')
+        ax.set_ylabel('Distance (km)')
+        plt.title(title)
+        plt.show()
+
+
 
 if __name__ == '__main__':
+    # Read in export from SBDB
     df = pd.read_csv('sbdb_query_results.csv')
     spkids = df.loc[df.neo == 'Y', 'spkid'].to_list()
     print(f'There are {len(spkids)} NEOs to process')
@@ -69,15 +82,3 @@ if __name__ == '__main__':
     neos = dist.check_approaches()
     df_out = pd.DataFrame(neos)
     df_out.to_csv('neos.csv', index=False)
-
-
-
-# # Plot the distance
-# fig, ax = plt.subplots()
-# ax.plot(neo_ephem['datetime_jd'], distance, 'k')
-# ax.set_xlabel('Time (JD)')
-# ax.set_ylabel('Distance (km)')
-# # Mean distance from earth
-# plt.axhline(mean_y, linestyle='--', color='gray')
-# plt.title("Distance from Earth to Moon")
-# plt.show()
